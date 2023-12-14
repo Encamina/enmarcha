a9c5d62329b410ddefd131caee97a16884518186
Luis Marcos Rivera <luis.m.r@outlook.com>
2023-12-14 13:35:50 +0100

    Fixed PR comments

--- src/Encamina.Enmarcha.SemanticKernel.Abstractions/ILengthFunctions.cs
+++ src/Encamina.Enmarcha.SemanticKernel.Abstractions/ILengthFunctions.cs
@@ -6,14 +6,14 @@ namespace Encamina.Enmarcha.SemanticKernel.Abstractions;
 public interface ILengthFunctions : AI.Abstractions.ILengthFunctions
 {
     /// <summary>
+    /// Gets the default <see cref="GptEncoding">encoding</see> for models like `GPT-3.5-Turbo` and `GPT-4` from OpenAI.
+    /// </summary>
+    public static readonly GptEncoding DefaultGptEncoding = GptEncoding.GetEncoding("cl100k_base");
+
+    /// <summary>
     /// Dictionary to cache GptEncoding instances based on encoding names.
     /// </summary>
     private static readonly Dictionary<string, GptEncoding> EncodingCache = [];
-
-    /// <summary>
-    /// Gets the default GptEncoding for models like `GPT-3.5-Turbo` and `GPT-4` from OpenAI.
-    /// </summary>
-    private static GptEncoding defaultGptEncoding;
 
     /// <summary>
     /// Gets the number of tokens using encodings for models like `GPT-3.5-Turbo` and `GPT-4` from OpenAI on the specified text.
@@ -21,7 +21,7 @@ public interface ILengthFunctions : AI.Abstractions.ILengthFunctions
     /// </summary>
     /// <seealso href="https://platform.openai.com/tokenizer"/>
     /// <seealso href="https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb"/>
-    public static Func<string, int> LengthByTokenCount => (text) => string.IsNullOrEmpty(text) ? 0 : GetDefaultGptEncoding().Encode(text).Count;
+    public static Func<string, int> LengthByTokenCount => (text) => string.IsNullOrEmpty(text) ? 0 : DefaultGptEncoding.Encode(text).Count;
 
     /// <summary>
     /// Gets the number of tokens using a given encoding on the specified text.
@@ -29,15 +29,6 @@ public interface ILengthFunctions : AI.Abstractions.ILengthFunctions
     /// </summary>
     /// <seealso href="https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb"/>
     public static Func<string, string, int> LengthByTokenCountUsingEncoding => (encoding, text) => string.IsNullOrEmpty(text) ? 0 : GetCachedEncoding(encoding).Encode(text).Count;
-
-    /// <summary>
-    /// Gets the default GptEncoding instance, creating it if not already initialized.
-    /// </summary>
-    /// <returns>The default GptEncoding instance.</returns>
-    private static GptEncoding GetDefaultGptEncoding()
-    {
-        return defaultGptEncoding ??= GptEncoding.GetEncoding("cl100k_base");
-    }
 
     /// <summary>
     /// Gets the GptEncoding instance based on the specified encoding name, caching it for future use.
