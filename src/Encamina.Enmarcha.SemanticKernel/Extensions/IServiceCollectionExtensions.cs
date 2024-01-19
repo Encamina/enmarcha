a5b5e8489948dd1ca85156b1a97f3b62bf96460a
Rodrigo Liberoff Vázquez <rliberoff@encamina.com>
2024-01-19 10:42:45 +0100

    Modified dependency injection lifetime for `MemoryManager` and `EphemeralMemoryStoreHandler` services.

--- src/Encamina.Enmarcha.SemanticKernel/Extensions/IServiceCollectionExtensions.cs
+++ src/Encamina.Enmarcha.SemanticKernel/Extensions/IServiceCollectionExtensions.cs
@@ -1,6 +1,4 @@
-﻿using CommunityToolkit.Diagnostics;
-
-using Encamina.Enmarcha.SemanticKernel;
+﻿using Encamina.Enmarcha.SemanticKernel;
 using Encamina.Enmarcha.SemanticKernel.Abstractions;
 using Encamina.Enmarcha.SemanticKernel.Options;
 
@@ -8,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Memory;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -18,21 +15,28 @@ namespace Microsoft.Extensions.DependencyInjection;
 public static class IServiceCollectionExtensions
 {
     /// <summary>
-    /// Adds and configures the <see cref="MemoryManager"/> type as an scoped instance of the <see cref="IMemoryManager"/> service to the <see cref="IServiceCollection"/>.
+    /// Adds and configures the <see cref="MemoryManager"/> type as transient service instance of the <see cref="IMemoryManager"/> service to the <see cref="IServiceCollection"/>.
     /// </summary>
+    /// <remarks>
+    /// This method registers the <see cref="MemoryManager"/> type as a transient service due to its dependency on <see cref="Kernel"/>, which is typically registered as transient or scoped (rarely as singleton).
+    /// </remarks>
     /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
     /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
     public static IServiceCollection AddMemoryManager(this IServiceCollection services)
     {
-        services.TryAddScoped<IMemoryManager, MemoryManager>();
+        services.TryAddTransient<IMemoryManager, MemoryManager>();
 
         return services;
     }
 
     /// <summary>
-    /// Adds and configures an «Ephemeral Memory Store Handler», which removes collections from memory after a configured time of inactivity (thus ephemeral), as a singleton
+    /// Adds and configures an «Ephemeral Memory Store Handler», which removes collections from memory after a configured time of inactivity (thus ephemeral), as a transient
     /// instance of the <see cref="IMemoryStoreHandler"/> service to the <see cref="IServiceCollection"/>.
     /// </summary>
+    /// <remarks>
+    /// This method registers the <see cref="EphemeralMemoryStoreHandlerOptions"/> type as a transient service because of its dependency on <see cref="IMemoryManager"/>s, which depend on <see cref="Kernel"/>, typically a
+    /// transient or scoped service (rarely a singleton).
+    /// </remarks>
     /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
     /// <param name="configuration">The current set of key-value application configuration parameters.</param>
     /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
@@ -43,7 +47,7 @@ public static class IServiceCollectionExtensions
                 .ValidateDataAnnotations()
                 .ValidateOnStart();
 
-        services.TryAddSingleton<IMemoryStoreHandler, EphemeralMemoryStoreHandler>();
+        services.TryAddTransient<IMemoryStoreHandler, EphemeralMemoryStoreHandler>();
 
         return services;
     }
