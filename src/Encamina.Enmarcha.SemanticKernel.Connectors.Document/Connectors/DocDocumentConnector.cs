b52e02a32d485c8f3eec4a662a30a5fa4bcb7ec7
Víctor Muñoz Molina <vmunoz@encamina.com>
2024-11-18 16:19:22 +0100

    Added two document connectors to SemanticKernel.Connectors.Document: `DocDocumentConnector` for ´.doc´ files and `HtmlDocumentConnector` for `.html` files.

---
+++ src/Encamina.Enmarcha.SemanticKernel.Connectors.Document/Connectors/DocDocumentConnector.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+using CommunityToolkit.Diagnostics;
+
+using NPOI.HWPF;
+using NPOI.HWPF.Extractor;
+
+namespace Encamina.Enmarcha.SemanticKernel.Connectors.Document.Connectors;
+
+/// <summary>
+/// Extracts text from a document in the <c>.doc</c> format.
+/// </summary>
+public class DocDocumentConnector : IEnmarchaDocumentConnector
+{
+    /// <inheritdoc/>
+    public IReadOnlyList<string> CompatibleFileFormats => [".DOC"];
+
+    /// <inheritdoc/>
+    public virtual string ReadText(Stream stream)
+    {
+        Guard.IsNotNull(stream);
+
+        // Register the code pages encoding provider for the .doc files
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        var document = new HWPFDocument(stream);
+
+        var extractor = new WordExtractor(document);
+
+        return extractor.Text.Trim();
+    }
+
+    /// <inheritdoc/>
+    public virtual void Initialize(Stream stream)
+    {
+        // Intentionally not implemented to comply with the Liskov Substitution Principle...
+    }
+
+    /// <inheritdoc/>
+    public virtual void AppendText(Stream stream, string text)
+    {
+        // Intentionally not implemented to comply with the Liskov Substitution Principle...
+    }
+}
